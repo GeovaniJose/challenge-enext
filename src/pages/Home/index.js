426d370223ee8c03d73e755f25389dc9309ec8c2
Geovani José <geovani-lol@hotmail.com>
2020-06-03 17:40:20 -0300

    Implement Home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import FormBreed from '../../components/FormBreed'
+
+import './styles.scss'
+
+const Home = () => {
+  return (
+    <main className="main-container">
+      <FormBreed />
+    </main>
+  )
+}
+
+export default Home
